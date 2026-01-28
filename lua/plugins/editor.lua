56aa0e834c553784460f876dfedd8ddf6c96a911
jamesespericueta <espericueta2303@gmail.com>
2026-01-27 19:45:04 -0500

    java

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -20,6 +20,14 @@ return {
   -- },
 
 {
+  'nvim-java/nvim-java',
+  config = function()
+    require('java').setup()
+    vim.lsp.enable('jdtls')
+  end,
+},
+
+{
   "vyfor/cord.nvim",
   build = ':Cord update',
   opts = {
